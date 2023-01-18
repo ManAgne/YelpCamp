337b5dd48bef538c86172280eb55af44d99e445b
managne <agne.manaikaite@gmail.com>
2023-01-18 20:54:31 +0200

    feat: added logged in after registered feature

--- routes/users.js
+++ routes/users.js
@@ -13,8 +13,11 @@ router.post('/register', catchAsync(async (req, res) => {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
-        req.flash('success', 'Welcome to Yelp Camp');
-        res.redirect('/campgrounds');
+        req.login(registeredUser, err => {
+            if(err) return next(err);
+            req.flash('success', 'Welcome to Yelp Camp');
+            res.redirect('/campgrounds');
+        });
     } catch (e) {
         req.flash('error', e.message);
         res.redirect('/register');
@@ -32,9 +35,7 @@ router.post('/login', passport.authenticate('local', { failureFlash: true, failu
 
 router.get('/logout', (req, res) => {
     req.logout((err) =>{
-        if (err) {
-            return next(err);
-        }
+        if (err) return next(err);
         req.flash('success', 'Buh bye :(')
         res.redirect('/campgrounds');
     });
